ca5b2d3d302cbb1547441bcaeba38bfc5a89c932
Takuya <dyhpaax@gmail.com>
2021-07-31 20:28:51 +0900

    personモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people', to: 'people#index'
+
   get 'msgboard/index'
   post 'msgboard/index'
   get 'msgboard', to: 'msgboard#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create(name: 'Taro', age: 38, mail:'Taro@example.com')
+Person.create(name: 'Mika', age: 30, mail: 'Mika@example.com')
+Person.create(name: 'sachiko', age: 25, mail: 'sachiko@example')
