31b28e3fd491a80e27f822c05ee794ae62b31eea
Takuya <dyhpaax@gmail.com>
2021-07-31 22:07:25 +0900

    データの新規作成ページ

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -8,4 +8,21 @@ class PeopleController < ApplicationController
     @msg = "Indexed data"
     @data = Person.find(params[:id])
   end
+
+  def add
+    @msg = "add new data"
+    @person = Person.new
+  end
+
+  def create
+    if request.post?
+      obj = Person.create(person_params)
+    end
+    redirect_to '/people'
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:name, :age, :mail)
+  end
 end
