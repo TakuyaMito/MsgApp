bc2bf47d926473489005231a02011a7d8dc832e4
Takuya <dyhpaax@gmail.com>
2021-08-01 11:18:18 +0900

    検索機能

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,5 @@
 class PeopleController < ApplicationController
+
   def index
     @msg = 'Person,data'
     @data = Person.all
@@ -36,6 +37,15 @@ class PeopleController < ApplicationController
     obj = Person.find(params[:id])
     obj.destroy
     redirect_to '/people'
+  end
+
+  def find
+    @msg = 'please type search word..'
+    @people = Array.new
+    if request.post?
+      f = '%' + params[:find] + '%'
+      @people = Person.where "name like ? or mail like ?", f, f
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'people/edit/:id', to: 'people#edit'
   patch 'people/edit/:id', to: 'people#update'
   get 'people/delete/:id', to: 'people#delete'
+  get 'people/find'
+  post 'people/find'
   get 'people/:id', to: 'people#show'
 
 
